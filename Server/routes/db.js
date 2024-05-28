95ef44787a1ba48227ea5f5f1e04b423e128c714
1p22geo <1p22geodecki@gmail.com>
2024-05-28 11:48:17 +0200

    feat(login): added login route

--- Server/routes/db.js
+++ Server/routes/db.js
@@ -43,5 +43,37 @@ router.get('/signup', async (req, res) => {
   res.status(200).json({ message: 'User created', id: insert_result.insertedId });
 });
 
+router.get('/login', async (req, res) => {
+  const client = new mongo.MongoClient(url, {
+    serverApi: {
+      version: mongo.ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });;
+  await client.connect()
+  const db = client.db('dziura');
+  const users_collection = db.collection('users');
+
+  const result = await users_collection.findOne({ email: req.body.email });
+  if (!result) {
+    res.status(400).json({ message: 'login failed' });
+  }
+  const salt = result.password.salt
+  const hash = sha256(req.body.password + salt)
+  if (!(result.password.hash === hash)) {
+    res.status(400).json({ message: 'login failed' });
+  }
+
+  // login successful
+  const sessions_collection = db.collection('sessions');
+  const insert_result = await sessions_collection.insertOne({
+    user: result._id,
+    start: Date.now() / 1000,
+    expires: Date.now() / 1000 + 3600_0000 // one hour long sessions
+  })
+
+  res.status(200).json({ message: 'login successful', id: insert_result.insertedId });
+})
 
 module.exports = router;
