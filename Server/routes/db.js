c16a983c2654f3d4ab65f7e23f758c5ccc5c18d2
michaspie8 <michaspie8@gmail.com>
2024-05-28 13:28:05 +0200

    Add requests router

--- Server/routes/db.js
+++ Server/routes/db.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true});
 
 const signupRoute = require("./auth/signup");
 const loginRoute = require("./auth/login");
+const requestRouter = require("./requests/requests");
 
 //docs at /doc/db/index.md
 
-router.get("/signup", signupRoute);
+router.post("/signup", signupRoute);
 
-router.get("/login", loginRoute);
+router.post("/login", loginRoute);
+
+router.use("/request", requestRouter);
+
+
+
+
+
 
 module.exports = router;
