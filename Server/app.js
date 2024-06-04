f30cc524ed00c2edab042e110561ed9fb0c6e318
1p22geo <1p22geodecki@gmail.com>
2024-06-04 14:31:53 +0200

    test: added Jest tests for login and register too

--- Server/app.js
+++ Server/app.js
@@ -46,6 +46,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
-//test routes
-const test = require("./routes/test");
--- Server/routes/auth/signup.js
+++ Server/routes/auth/signup.js
@@ -4,7 +4,7 @@ const url = process.env.DZIURA_DB;
 const db_utilities = require("../../lib/db_utilities");
 
 module.exports = async (req, res) => {
-  const db = await db_utilities.get_db();
+  const { db, client } = await db_utilities.get_db();
   const users_collection = db.collection("users");
   //check if user exists
   const result = await users_collection.findOne({
@@ -33,6 +33,7 @@ module.exports = async (req, res) => {
     social_links: [],
   };
   const insert_result = await users_collection.insertOne(user);
+  await client.close()
   res
     .status(200)
     .json({ message: "User created", id: insert_result.insertedId });
