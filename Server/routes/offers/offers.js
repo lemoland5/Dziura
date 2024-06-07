50bc795c48928dab748b554992a275024da4376a
Geodecki 2P gr.1 <1p22geodecki@gmail.com>
2024-06-07 08:25:23 +0200

    fix: close connections and update imports

--- Server/routes/offers/offers.js
+++ Server/routes/offers/offers.js
@@ -1,44 +1,9 @@
-/*```ts
-{
-  "_id": ObjectID, // ID of request
-  "user": ObjectID, // who posted it
-  "created": number, // UNIX timestamp - date of creation
-  "title": string, // title of the request
-  "subject": string, // school subject, must be one of the available subjects, is checked on backend
-  "topic": ObjectID|undefined, // optional topic ID
-  "content": string, // content of the request, as typed by user
-  "daterange": {
-    "from": number, // since when is the offer applicable
-    "to": number, // until when will it be
-  },
-  "attachments":{
-      "filename": string, // name of the attachment
-      "url": string, // URL of attachment after uploading
-      "embed": "image"|"video"|null, // should it be embedded in the webpage as a preview or not
-      "mimetype": string, // MIME type of the uploaded file
-  }[],
-  "comments": {
-    "author": ObjectID, // original author
-    "content": string, // contents as string
-    "created": number, // UNIX timestamp of creation
-    "votes":{
-      "up": number, // upvotes (or "likes")
-      "down": number, // downvotes (or "dislikes")
-    }
-  }[],
-  "ratings": {
-    "author": ObjectID, // who gave the rating, might not be necessary
-    "rating": number, // number of points awarded or stars rated
-  }
-}
-``` */
 const mongo = require("mongodb");
-const url = process.env.DZIURA_DB;
 const db_utilities = require("../../lib/db_utilities");
 const router = require("express").Router({ mergeParams: true });
 
 router.post("/", async (req, res) => {
-  const db = await db_utilities.get_db();
+  const {db, client} = await db_utilities.get_db();
   const offers_collection = db.collection("offers");
   const sessions_collection = db.collection("sessions");
   const id = new mongo.ObjectId(req.cookies.session);
@@ -63,11 +28,12 @@ router.post("/", async (req, res) => {
     ratings: [],
   };
   const result = await offers_collection.insertOne(offer);
+  client.close()
   res.status(200).json(result);
 });
 
 router.get("/", async (req, res) => {
-  const db = await db_utilities.get_db();
+  const {db, client} = await db_utilities.get_db();
   const offers_collection = db.collection("offers");
   const limit = req.query.limit ? parseInt(req.query.limit) : 5;
   const page = req.query.page ? parseInt(req.query.page) : 0;
@@ -77,11 +43,12 @@ router.get("/", async (req, res) => {
     .skip(page * limit)
     .limit(limit)
     .toArray();
+  client.close()
   res.status(200).json(result);
 });
 
 router.get("id/:id", async (req, res) => {
-  const db = await db_utilities.get_db();
+  const {db, client} = await db_utilities.get_db();
   const offers_collection = db.collection("offers");
   const result = await offers_collection.findOne({
     _id: new mongo.ObjectId(req.params.id),
@@ -90,11 +57,12 @@ router.get("id/:id", async (req, res) => {
     res.status(404).json({ message: "Offer not found" });
     return;
   }
+  client.close()
   res.status(200).json(result);
 });
 
 router.get("user/:id", async (req, res) => {
-  const db = await db_utilities.get_db();
+  const {db, client} = await db_utilities.get_db();
   const offers_collection = db.collection("offers");
   const result = await offers_collection
     .find({
@@ -105,6 +73,7 @@ router.get("user/:id", async (req, res) => {
     res.status(404).json({ message: "Offer not found" });
     return;
   }
+  client.close()
   res.status(200).json(result);
 });
 
