a8629921dc75feb75d80f5fe5449ab1b11cc89e5
Krzysztof Woszek <k.woszek@outlook.com>
2024-09-29 12:04:55 +0200

    Admin panel Nowi userzy pojawiaja sie na admin panelu i mozna ich zatwierdzac

--- Server/routes/admin/adminRouter.js
+++ Server/routes/admin/adminRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db_utils = require("../../lib/db_utilities");
 const session_utils = require("../../lib/session_utilities")
+const {ObjectId} = require("mongodb");
 router.get("/", async (req, res) => {
     const {db, client} = await db_utils.get_db();
 
@@ -10,23 +11,36 @@ router.get("/", async (req, res) => {
     }
     const {session, user} = await session_utils.get_user_data(req.cookies.session, db)
 
-    if(user.type != 'admin'){
+    if(user.type !== 'admin'){
         res.redirect('/profile')
     }
-    console.log(user);
-//   const session = await db
+    //   const session = await db
 //   .collection("sessions")
 //   .findOne({ _id: new mongodb.ObjectId(req.cookies.session.toString()) });
 //     const user = await db
 //   .collection("users")
 //   .findOne({ _id: new mongodb.ObjectId(session.user) });
 
-    const admin = user.type == 'admin';
+    const getUnverifiedUsers = await db.collection("users").find({verified: false})
+
+    let unverifiedUsers = Array()
+
+    for await (const doc of getUnverifiedUsers) {
+        unverifiedUsers.push(doc)
+    }
 
 
-
-    // res.render("./admin/admin.ejs");
-    res.json({message : "Halo admin"})
+    res.render("./admin/admin.ejs", {data: unverifiedUsers});
+    await  client.close()
 });
+
+router.post('/', async(req,res)=>{
+    const {db, client} = await db_utils.get_db();
+    const users = await db.collection('users')
+    console.log(req.body.user)
+    await users.findOneAndUpdate({_id: new ObjectId(req.body.user)}, {$set: {verified: true}})
+    res.redirect('/admin')
+    await client.close()
+})
 
 module.exports = router;
